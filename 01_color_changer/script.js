442a3249107ca3daea7173451ad6969d8e49878e
RahulSinghAswal <aswalrahul576@gmail.com>
2024-06-09 01:51:41 +0530

    project-1 color changer

---
+++ 01_color_changer/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".button");
+
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  //   console.log(button);
+  button.addEventListener("click", (e) => {
+    // console.log(e);
+    // console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "aqua") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "orange") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
